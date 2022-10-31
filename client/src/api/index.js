bbf3a9200e4e19ce2c32537cb59b482c80940af0
Moeed <moeedawan2121@gmail.com>
2022-10-31 05:26:03 +0500

    remote monog

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,26 +1,28 @@
-import axios from 'axios';
+import axios from "axios";
 
-
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({
+  baseURL: "ec2-13-114-33-203.ap-northeast-1.compute.amazonaws.com:5000",
+});
 
 API.interceptors.request.use((req) => {
-    if(localStorage.getItem('profile')){
-        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
-    }
+  if (localStorage.getItem("profile")) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("profile")).token
+    }`;
+  }
 
-    return req;
-})
-
+  return req;
+});
 
 // const url = 'https://memor1es.herokuapp.com/posts';
 // const url = 'http://localhost:5000/posts';
 
-export const fetchPosts = () => API.get('/posts');
-export const createPost = (newPost) => API.post('/posts', newPost);
-export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
+export const fetchPosts = () => API.get("/posts");
+export const createPost = (newPost) => API.post("/posts", newPost);
+export const updatePost = (id, updatedPost) =>
+  API.patch(`/posts/${id}`, updatedPost);
 export const deletePost = (id) => API.delete(`/posts/${id}`);
 export const likePost = (id) => API.patch(`/posts/${id}/${"likePost"}`);
 
-
-export const signIn = (formData) => API.post('/users/signin', formData);
-export const signUp = (formData) => API.post('/users/signup', formData);
+export const signIn = (formData) => API.post("/users/signin", formData);
+export const signUp = (formData) => API.post("/users/signup", formData);
